61d210d72b8b5de47ab25941a1842c97d343fff1
bzrbkv <60747374+bzrbkv@users.noreply.github.com>
2020-06-28 10:23:49 -0700

    Greeter App

---
+++ Greeter/app/src/main/java/com/example/greeter/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.greeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void didTapGreetButton(View view) {
+        EditText greetEditText = (EditText) findViewById(R.id.greetEditText);
+
+        String name = greetEditText.getText().toString();
+        String greeting = String.format("Hello, %s!", name);
+
+        TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
+
+        messageTextView.setText(greeting);
+    }
+}
